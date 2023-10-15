0b86df67754e730f1051a488534091a41669c788
Fawl3r <Fawl3r@gmail.com>
2023-10-15 12:52:54 -0500

    up

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,4 +1,4 @@
 export const editionDropContractAddress = "0xB8a33200A4a09d8c5F5313D6a19A9609B916479e";
-export const tokenContractAddress = "0x7175F2C267cEfB8cb57Ef1aE8711bD84B11e61E3";
-export const stakingContractAddress = "0x6200DC292Daa2E6A3fF3d5523C9b0B252Cd2Cc71";
+export const tokenContractAddress = "0x886c4C3149983D4Dbe13e251c95230A61b89F78c";
+export const stakingContractAddress = "0x317F6C70FCE1E63F6cee75661348D4521858b171";
  
