ded2a421136c5ca315012aa0197506bc67bf96d8
rameshvoodi <rameshkumarvoodi2002@gmail.com>
2023-10-18 23:44:10 +0530

    ft-final-erc115

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,6 +1,7 @@
 import {
   ThirdwebNftMedia,
   useContract,
+  useContractRead,
   useNFT,
   Web3Button,
 } from "@thirdweb-dev/react";
@@ -8,16 +9,24 @@ import type { FC } from "react";
 import {
   editionDropContractAddress,
   stakingContractAddress,
+  locktime,
 } from "../consts/contractAddresses";
 import styles from "../styles/Home.module.css";
 
 interface NFTCardProps {
   tokenId: number;
+  address: string;
 }
 
-const NFTCard: FC<NFTCardProps> = ({ tokenId }) => {
+const NFTCard: FC<NFTCardProps> = ({ tokenId, address }) => {
   const { contract } = useContract(editionDropContractAddress, "edition-drop");
+  const { contract: stakingContract } = useContract(stakingContractAddress);
   const { data: nft } = useNFT(contract, tokenId);
+  const { data: getstakeInfoOfToken } = useContractRead(
+    stakingContract,
+    "getStakeInfoForToken",
+    [tokenId, address]
+  );
 
   return (
     <>
@@ -38,6 +47,34 @@ const NFTCard: FC<NFTCardProps> = ({ tokenId }) => {
           >
             Withdraw
           </Web3Button>
+          <br />
+          <br />
+          {getstakeInfoOfToken &&
+          getstakeInfoOfToken[1].toNumber() != 0 &&
+          getstakeInfoOfToken[1].toNumber() * 3600 >= locktime ? (
+            <Web3Button
+              action={(contract) =>
+                contract?.call("claimRewards", [nft.metadata.id])
+              }
+              contractAddress={stakingContractAddress}
+            >
+              Claim Rewards
+            </Web3Button>
+          ) : (
+            <Web3Button
+              action={() =>
+                window.alert(
+                  `You can't claim rewards yet, please wait for ${
+                    locktime / 3600 - getstakeInfoOfToken[1].toNumber()
+                  } hours`
+                )
+              }
+              contractAddress={stakingContractAddress}
+            >
+              claim in {locktime / 3600 - getstakeInfoOfToken[1].toNumber()}{" "}
+              hours
+            </Web3Button>
+          )}
         </div>
       )}
     </>
